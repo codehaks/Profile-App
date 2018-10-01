772aa5724a64fde7492bcefad656c5f207d9bb64
Hakim Ghods <ghods.hakim@hotmail.com>
2018-10-01 23:08:30 +0330

    Create

--- ProfileApp/Pages/Profile/Create.cshtml.cs
+++ ProfileApp/Pages/Profile/Create.cshtml.cs
@@ -2,22 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Codehaks.Extentions.LiteDb;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProfileApp.Models;
 
 namespace ProfileApp.Pages.Profile
 {
     [BindProperties]
     public class CreateModel : PageModel
     {
+        private readonly LiteDbContext _db;
+        public CreateModel(LiteDbContext db)
+        {
+            _db = db;
+        }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int ProvinceId { get; set; }
         public int CityId { get; set; }
 
-        public void OnGet()
+        public IActionResult OnPost()
         {
+            var profiles=_db.Context.GetCollection<UserProfile>();
 
+            profiles.Insert(new UserProfile()
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                CityId = CityId,
+                ProvinceId = ProvinceId,
+                TimeCreated = DateTime.Now
+            });
+
+            return RedirectToPage("./Index");
         }
     }
 }
